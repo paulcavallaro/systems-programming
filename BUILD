df1b1a63fb39c06f31e68544cda0086884e41674
Paul Cavallaro <paulcavallaro@gmail.com>
2019-08-09 23:03:46 -0400

    Initial commit to setup bazel and absl

---
+++ BUILD
@@ -0,0 +1,5 @@
+cc_binary(
+  name = "hello_world",
+  deps = ["@com_google_absl//absl/strings"],
+  srcs = ["hello_world.cc"],
+)
---
+++ hello_world.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "absl/strings/str_join.h"
+
+int main() {
+  std::vector<std::string> v = {"foo","bar","baz"};
+  std::string s = absl::StrJoin(v, "-");
+
+  std::cout << "Joined string: " << s << "\n";
+
+  return(0);
+}
