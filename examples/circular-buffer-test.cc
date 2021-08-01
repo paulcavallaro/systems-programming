c16c4941ae4d53c8f793874879292112a1a8a248
Paul Cavallaro <paulcavallaro@gmail.com>
2021-08-01 16:13:04 -0400

    [circular-buffer] Add a more fleshed out benchmark comparison between a circular buffer and std::queue

--- examples/circular-buffer-test.cc
+++ examples/circular-buffer-test.cc
@@ -1,19 +1,65 @@
 #include "examples/circular-buffer.h"
 
+#include <queue>
+
 #include "benchmark/benchmark.h"
 
 namespace sysprog {
 namespace {
 
-void BM_CircularBuffer(benchmark::State& state) {
-  CircularBuffer<int> buffer(1024);
+constexpr size_t kBufferSize = 2048;
+
+void BM_CircularBuffer_InsertAndPop(benchmark::State& state) {
+  CircularBuffer<int> buffer(kBufferSize);
   for (auto _ : state) {
-    buffer.push_back(0);
-    benchmark::DoNotOptimize(buffer.pop_front());
+    for (int i = 0; i < kBufferSize - 1; i++) {
+      buffer.push_back(i);
+    }
+    while (!buffer.empty()) {
+      buffer.pop_front();
+    }
+    benchmark::DoNotOptimize(buffer);
   }
 }
 
-BENCHMARK(BM_CircularBuffer);
+void BM_CircularBuffer_Insert(benchmark::State& state) {
+  CircularBuffer<int> buffer(kBufferSize);
+  for (auto _ : state) {
+    for (int i = 0; i < kBufferSize - 1; i++) {
+      buffer.push_back(i);
+    }
+  }
+  benchmark::DoNotOptimize(buffer);
+}
+
+void BM_StdQueue_InsertAndPop(benchmark::State& state) {
+  std::queue<int> buffer;
+  for (auto _ : state) {
+    for (int i = 0; i < kBufferSize - 1; i++) {
+      buffer.push(i);
+    }
+    while (!buffer.empty()) {
+      buffer.pop();
+    }
+    benchmark::DoNotOptimize(buffer);
+  }
+}
+
+void BM_StdQueue_Insert(benchmark::State& state) {
+  for (auto _ : state) {
+    std::queue<int> buffer;
+    for (int i = 0; i < kBufferSize - 1; i++) {
+      buffer.push(i);
+    }
+    benchmark::DoNotOptimize(buffer);
+  }
+}
+
+BENCHMARK(BM_CircularBuffer_InsertAndPop);
+BENCHMARK(BM_CircularBuffer_Insert);
+
+BENCHMARK(BM_StdQueue_InsertAndPop);
+BENCHMARK(BM_StdQueue_Insert);
 
 }  // namespace
 }  // namespace sysprog
