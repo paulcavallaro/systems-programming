c20a48c00d7fb257a7ecbab08cdcca8efc8438d6
Paul Cavallaro <paulcavallaro@gmail.com>
2021-08-02 20:30:28 -0400

    [glog] Upgrade glog to 0.5.0 release

--- WORKSPACE
+++ WORKSPACE
@@ -11,9 +11,9 @@ http_archive(
 # glog archive.
 http_archive(
   name = "com_github_google_glog",
-  urls = ["https://github.com/google/glog/archive/ba8a9f6952d04d1403b97df24e6836227751454e.zip"],
-  strip_prefix = "glog-ba8a9f6952d04d1403b97df24e6836227751454e",
-  sha256 = "9b4867ab66c33c41e2672b5de7e3133d38411cdb75eeb0d2b72c88bb10375c71",
+  urls = ["https://github.com/google/glog/archive/refs/tags/v0.5.0.zip"],
+  strip_prefix = "glog-0.5.0",
+  sha256 = "21bc744fb7f2fa701ee8db339ded7dce4f975d0d55837a97be7d46e8382dea5a",
 )
 
 # gflags archive.
--- examples/hello-world.cc
+++ examples/hello-world.cc
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 #include "absl/strings/str_join.h"
+#include "glog/logging.h"
 
-int main() {
-  std::vector<std::string> v = {"foo","bar","baz"};
-  std::string s = absl::StrJoin(v, "-");
+int main(int argc, char* argv[]) {
+  // Initialize Google’s logging library.
+  google::InitGoogleLogging(argv[0]);
 
-  std::cout << "Joined string: " << s << "\n";
+  // Join some strings
+  std::vector<std::string> v = {"foo", "bar", "baz"};
+  std::string s = absl::StrJoin(v, ", ");
 
-  return(0);
+  LOG(ERROR) << "Joined string: " << s << "\n";
+
+  return 0;
 }
